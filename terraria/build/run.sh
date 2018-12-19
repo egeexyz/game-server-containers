0cd05838812e8583966579fce12d6e77c6bc3e71
Omegapex <trashmcthrash@gmail.com>
2018-12-19 13:08:23 -0500

    Switched to steamcmd

--- terraria/build/run.sh
+++ terraria/build/run.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-WORK_DIR="/home/gsc/server_files"
-cd $WORK_DIR
-if [ ! -f "$WORK_DIR/TerrariaServer.exe" ]; then
-  /usr/bin/update 4.3.25
-  echo "Install complete. Launching server now..."
-fi
-mono $WORK_DIR/TerrariaServer.exe -config $WORK_DIR/config.txt
+steamcmd +login anonymous +force_install_dir /home/gsc/server_files +app_update 105600 validate +quit
