7c3cf2e0f6108edeccbffd4a7397e64a204b86f4
egee-irl <brian@egee.io>
2019-01-03 23:49:56 -0800

    updated path to steamcmd

--- rust/build/update.sh
+++ rust/build/update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-steamcmd +login anonymous +force_install_dir /home/gsc/server_files +app_update 258550 validate +quit
+/usr/bin/steamcmd +login anonymous +force_install_dir /home/gsc/server_files +app_update 258550 validate +quit
